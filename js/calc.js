1bba66a3b20e2da19bb92f4c5c953b7ad5300014
3DMicks <91573410+3DMicks@users.noreply.github.com>
2023-02-26 16:37:20 -0300

    Add operation insertion logic

--- js/calc.js
+++ js/calc.js
@@ -7,12 +7,22 @@ function clearInput(event) {
 }
 
 function deleteInput(event) {
-    getInput().textContent = [...getInput().textContent].slice(0, -1).join("");
+    getInput().textContent = [...getInput().textContent.trim()].slice(0, -1).join("");
 }
 
 function insertNumber(event) {
     let input = getInput();
     input.textContent = input.textContent + this.getAttribute("data-value");
+}
+
+function insertOperation(event) {
+    let input = getInput();
+    let opIcons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let trimmedInput = [...input.textContent.trim()];
+    console.log(trimmedInput);
+    if (!opIcons.includes(trimmedInput[trimmedInput.length - 1])) return;
+
+    input.textContent = `${input.textContent} ${this.operation} `;
 }
 
 document.querySelectorAll(".calc-btn.number").forEach((btn) => {
@@ -22,3 +32,19 @@ document.querySelectorAll(".calc-btn.number").forEach((btn) => {
 document.querySelector("#btn-clear").addEventListener("click", clearInput);
 
 document.querySelector("#btn-delete").addEventListener("click", deleteInput);
+
+let btnAdd = document.querySelector("#btn-add");
+btnAdd.addEventListener("click", insertOperation);
+btnAdd.operation = "+";
+
+let btnSub = document.querySelector("#btn-subtract");
+btnSub.addEventListener("click", insertOperation);
+btnSub.operation = "-";
+
+let btnDivide = document.querySelector("#btn-divide");
+btnDivide.addEventListener("click", insertOperation);
+btnDivide.operation = "/";
+
+let btnMultiply = document.querySelector("#btn-multiply");
+btnMultiply.addEventListener("click", insertOperation);
+btnMultiply.operation = "*";
