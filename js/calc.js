f64e8cddf35db07158fc29e33992ad7b431950bc
3DMicks <91573410+3DMicks@users.noreply.github.com>
2023-02-26 17:14:52 -0300

    Improve evaluation logic

--- js/calc.js
+++ js/calc.js
@@ -15,22 +15,42 @@ function insertNumberEvent(event) {
     input.textContent = input.textContent + this.getAttribute("data-value");
 }
 
-function insertOperationEvent(event) {
+function isFinalCharValid() {
     let input = getInput();
-    let opIcons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let trimmedInput = [...input.textContent.trim()];
 
-    if (!opIcons.includes(trimmedInput[trimmedInput.length - 1])) return;
+    return (digits.includes(trimmedInput[trimmedInput.length - 1]));
+}
+
+function insertOperationEvent(event) {
+    let input = getInput();
+    if (!isFinalCharValid()) return;
 
     input.textContent = `${input.textContent} ${this.operation} `;
 }
 
 function operate(event) {
     let input = getInput();
-    let result = 69420;
+    if (input.textContent.length <= 0) return;
+    if (!isFinalCharValid()) return;
 
-    clearInput();
-    input.textContent = result.toString();
+    let result = 69420;
+    let opArray = [...input.textContent.trim()].filter((char) => char != " ");
+
+    const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const symbols = ["+", "-", "÷", "x"];
+
+    let operations = [];
+    for (const [index, char] of opArray.entries()) {
+        if (symbols.includes(char)) operations.push(index);
+    }
+
+    console.log(opArray);
+    console.log(operations);
+
+    //clearInput();
+    //input.textContent = result.toString();
 }
 
 document.querySelectorAll(".calc-btn.number").forEach((btn) => {
@@ -51,10 +71,10 @@ btnSub.operation = "-";
 
 let btnDivide = document.querySelector("#btn-divide");
 btnDivide.addEventListener("click", insertOperationEvent);
-btnDivide.operation = "/";
+btnDivide.operation = "÷";
 
 let btnMultiply = document.querySelector("#btn-multiply");
 btnMultiply.addEventListener("click", insertOperationEvent);
-btnMultiply.operation = "*";
+btnMultiply.operation = "x";
 
 document.querySelector("#btn-operate").addEventListener("click", operate);
