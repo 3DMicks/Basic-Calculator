1a5ea2c803bc389b9803bd4295b2a099b66ca1c3
3DMicks <91573410+3DMicks@users.noreply.github.com>
2023-02-26 16:46:38 -0300

    Add initial operate function

--- js/calc.js
+++ js/calc.js
@@ -2,31 +2,39 @@ function getInput() {
     return document.querySelector(".calc-input");
 }
 
-function clearInput(event) {
+function clearInput() {
     getInput().textContent = "";
 }
 
-function deleteInput(event) {
+function deleteInput() {
     getInput().textContent = [...getInput().textContent.trim()].slice(0, -1).join("");
 }
 
-function insertNumber(event) {
+function insertNumberEvent(event) {
     let input = getInput();
     input.textContent = input.textContent + this.getAttribute("data-value");
 }
 
-function insertOperation(event) {
+function insertOperationEvent(event) {
     let input = getInput();
     let opIcons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let trimmedInput = [...input.textContent.trim()];
-    console.log(trimmedInput);
+
     if (!opIcons.includes(trimmedInput[trimmedInput.length - 1])) return;
 
     input.textContent = `${input.textContent} ${this.operation} `;
 }
 
+function operate(event) {
+    let input = getInput();
+    let result = 69420;
+
+    clearInput();
+    input.textContent = result.toString();
+}
+
 document.querySelectorAll(".calc-btn.number").forEach((btn) => {
-    btn.addEventListener("click", insertNumber);
+    btn.addEventListener("click", insertNumberEvent);
 });
 
 document.querySelector("#btn-clear").addEventListener("click", clearInput);
@@ -34,17 +42,19 @@ document.querySelector("#btn-clear").addEventListener("click", clearInput);
 document.querySelector("#btn-delete").addEventListener("click", deleteInput);
 
 let btnAdd = document.querySelector("#btn-add");
-btnAdd.addEventListener("click", insertOperation);
+btnAdd.addEventListener("click", insertOperationEvent);
 btnAdd.operation = "+";
 
 let btnSub = document.querySelector("#btn-subtract");
-btnSub.addEventListener("click", insertOperation);
+btnSub.addEventListener("click", insertOperationEvent);
 btnSub.operation = "-";
 
 let btnDivide = document.querySelector("#btn-divide");
-btnDivide.addEventListener("click", insertOperation);
+btnDivide.addEventListener("click", insertOperationEvent);
 btnDivide.operation = "/";
 
 let btnMultiply = document.querySelector("#btn-multiply");
-btnMultiply.addEventListener("click", insertOperation);
+btnMultiply.addEventListener("click", insertOperationEvent);
 btnMultiply.operation = "*";
+
+document.querySelector("#btn-operate").addEventListener("click", operate);
